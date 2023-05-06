563b7023f195c041b561fe3ff5076e305eecc9e9
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-05-06 19:00:31 +0300

    cli app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# node-js
+![list contacts command](https://ibb.co/vZsMXw1);
+![get contact command](https://ibb.co/LJPFkD8);
+![add contact command](https://ibb.co/BZnCr61);
+![remove contact command](https://ibb.co/wBrqWxS);
---
+++ index.js
@@ -0,0 +1,40 @@
+const { Command } = require("commander");
+const program = new Command();
+
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./db/contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      return console.log(allContacts);
+    case "get":
+      const contactById = await getContactById(id);
+      return console.log(contactById);
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      return console.log(newContact);
+    case "remove":
+      const contactToDelete = await removeContact(id);
+      return console.log(contactToDelete);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const argv = program.opts();
+invokeAction(argv);
